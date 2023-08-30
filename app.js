6e635709b3e3ab79fc934c229dd8df077511cf27
SAI KRISHNA S B <103356323+saigeek@users.noreply.github.com>
2023-08-30 19:23:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _=require("lodash");
 
